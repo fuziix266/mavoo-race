a6447b5965d24e22eb4beaa68d13667abc792792
fuziix266 <fuziix@gmail.com>
2026-03-20 17:53:03 -0300

    fix: Heat 4 con 3 atletas Pro, IDs Heat 5/6 corregidos (16-22), nombres con tildes

--- lib/models/heat.dart
+++ lib/models/heat.dart
@@ -28,25 +28,25 @@ class Heat {
     Heat(
       id: 1, nombre: 'Heat 1', categoria: 'Duplas Mujeres Amateur', horaInicio: '08:00',
       equipos: [
-        Team(id: 1, idHeat: 1, nombre: 'Imparables'),
-        Team(id: 2, idHeat: 1, nombre: 'Las Morenasas'),
-        Team(id: 3, idHeat: 1, nombre: 'Sinchi Warmi'),
-        Team(id: 4, idHeat: 1, nombre: 'Las 30&Fuertes'),
+        Team(id: 1,  idHeat: 1, nombre: 'Imparables'),
+        Team(id: 2,  idHeat: 1, nombre: 'Las Morenasas'),
+        Team(id: 3,  idHeat: 1, nombre: 'Sinchi Warmi'),
+        Team(id: 4,  idHeat: 1, nombre: 'Las 30&Fuertes'),
       ],
     ),
     Heat(
       id: 2, nombre: 'Heat 2', categoria: 'Duplas Mujeres Amateur', horaInicio: '08:30',
       equipos: [
-        Team(id: 5, idHeat: 2, nombre: 'Las Despist-hadas'),
-        Team(id: 6, idHeat: 2, nombre: 'Barbara y Nicol'),
-        Team(id: 7, idHeat: 2, nombre: 'Máquinas Primerizas'),
+        Team(id: 5,  idHeat: 2, nombre: 'Las Despist-hadas'),
+        Team(id: 6,  idHeat: 2, nombre: 'Bárbara y Nicol'),
+        Team(id: 7,  idHeat: 2, nombre: 'Máquinas Primerizas'),
       ],
     ),
     Heat(
       id: 3, nombre: 'Heat 3', categoria: 'Duplas Hombres Amateur', horaInicio: '10:00',
       equipos: [
-        Team(id: 8, idHeat: 3, nombre: 'Pinky y Cerebro'),
-        Team(id: 9, idHeat: 3, nombre: 'Team CMT'),
+        Team(id: 8,  idHeat: 3, nombre: 'Pinky y Cerebro'),
+        Team(id: 9,  idHeat: 3, nombre: 'Team CMT'),
         Team(id: 10, idHeat: 3, nombre: 'Los Hermanos Torres'),
         Team(id: 11, idHeat: 3, nombre: 'Mix Meta'),
         Team(id: 12, idHeat: 3, nombre: 'Team Eleven'),
@@ -54,22 +54,27 @@ class Heat {
     ),
     Heat(
       id: 4, nombre: 'Heat 4', categoria: 'Individual Hombres Pro', horaInicio: '12:00',
-      equipos: [],
+      equipos: [
+        Team(id: 13, idHeat: 4, nombre: 'Reinaldo Cuevas Nazal'),
+        Team(id: 14, idHeat: 4, nombre: 'Cristian Miranda Simpson'),
+        Team(id: 15, idHeat: 4, nombre: 'Roberto Ortega Ortega'),
+      ],
     ),
     Heat(
       id: 5, nombre: 'Heat 5', categoria: 'Duplas Mixtas Amateur', horaInicio: '14:00',
       equipos: [
-        Team(id: 13, idHeat: 5, nombre: 'Studio Indómito'),
-        Team(id: 14, idHeat: 5, nombre: 'Mix Meta Junior'),
-        Team(id: 15, idHeat: 5, nombre: 'Pulso Indomalku'),
+        Team(id: 16, idHeat: 5, nombre: 'Studio Indómito'),
+        Team(id: 17, idHeat: 5, nombre: 'Mix Meta Junior'),
+        Team(id: 18, idHeat: 5, nombre: 'Pulso Indomallku'),
+        Team(id: 19, idHeat: 5, nombre: 'Gene y Pato'),
       ],
     ),
     Heat(
       id: 6, nombre: 'Heat 6', categoria: 'Duplas Mixtas Amateur', horaInicio: '14:30',
       equipos: [
-        Team(id: 16, idHeat: 6, nombre: 'No Runners'),
-        Team(id: 17, idHeat: 6, nombre: 'Mix Meta'),
-        Team(id: 18, idHeat: 6, nombre: '40/20'),
+        Team(id: 20, idHeat: 6, nombre: 'No Runners'),
+        Team(id: 21, idHeat: 6, nombre: 'Mix Meta'),
+        Team(id: 22, idHeat: 6, nombre: '40/20'),
       ],
     ),
   ];
